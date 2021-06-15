d4d767ce255fe1aac841214ba9db8245a5f182f7
Rafael Leite <rl.oliveira321@gmail.com>
2021-06-15 04:00:29 -0300

    form style 2

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 
 // Form
 import { FaPlus } from 'react-icons/fa';
+// Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
 
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     novaTarefa: '',
+    tarefas: [
+      'Fazer café',
+      'Beber água',
+      'Estudar',
+    ],
   };
 
   inputMudou = (e) => {
@@ -17,7 +24,7 @@ export default class Main extends Component {
   }
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
@@ -33,6 +40,18 @@ export default class Main extends Component {
             <FaPlus />
           </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map(tarefa => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
